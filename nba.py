d56c76cc399fdd76a7635ae2754d7912b6641ce8
scott <scott.a.magnuson.19@dartmouth.edu>
2018-09-27 13:12:56 -0400

    Retrives necessary info from api

--- nba.py
+++ nba.py
@@ -1,29 +1,20 @@
-from nba_api.stats.endpoints import commonplayerinfo, boxscoreadvancedv2
+from nba_api.stats.endpoints import commonplayerinfo, playerfantasyprofile
+from nba_api.stats.static import players
 import pandas
 
-player_info = commonplayerinfo.CommonPlayerInfo(player_id=2544)
+playerName = 'Lebron James'
+playerId = players.find_players_by_full_name(playerName)[0]['id']
+print(playerId)
+
+seasonYear = '2015-16'
+player_info = commonplayerinfo.CommonPlayerInfo(player_id= playerId)
+playerInfo = player_info.common_player_info.get_data_frame()
+# print (avaliableSeasonDF.at[0, 'SEASON_ID'])
+print (playerInfo)
+# team_id =
 
 
-# Returns the data set in a pandas DataFrame.
-avaliableSeasonDF = player_info.available_seasons.get_data_frame()
-print (avaliableSeasonDF.at[0, 'SEASON_ID'])
+seasonInfo = playerfantasyprofile.PlayerFantasyProfile(player_id = playerId, season=seasonYear)
 
-playerInfoDF = player_info.common_player_info.get_data_frame()
-
-boxScoreInfo = boxscoreadvancedv2.BoxScoreAdvancedV2(game_id= "0021700807")
-
-boxScoreDF = boxScoreInfo.player_stats.get_data_frame()
-print(boxScoreDF)
-
-
-# # Find players by full name.
-# players.find_players_by_full_name('james')
-#
-# # Find players by first name.
-# players.find_players_by_first_name('lebron')
-#
-# # Find players by last name.
-# players.find_players_by_last_name('^(james|love)$')
-#
-# # Get all players.
-# players.get_players()
+seasonInfoByGame= seasonInfo.opponent.get_data_frame()
+print(seasonInfoByGame)
