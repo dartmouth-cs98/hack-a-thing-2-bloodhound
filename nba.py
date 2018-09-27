4ae1abdccdf8c4c884bc81fe3ae5e13cb6dd84ed
scott <scott.a.magnuson.19@dartmouth.edu>
2018-09-27 13:27:56 -0400

    updated functionality

--- nba.py
+++ nba.py
@@ -3,10 +3,12 @@ from nba_api.stats.static import players
 import pandas
 
 playerName = 'Lebron James'
+seasonYear = '2015-16'
+statCategory = 'GROUP_VALUE'
 playerId = players.find_players_by_full_name(playerName)[0]['id']
 print(playerId)
 
-seasonYear = '2015-16'
+
 player_info = commonplayerinfo.CommonPlayerInfo(player_id= playerId)
 playerInfo = player_info.common_player_info.get_data_frame()
 # print (avaliableSeasonDF.at[0, 'SEASON_ID'])
@@ -18,3 +20,5 @@ seasonInfo = playerfantasyprofile.PlayerFantasyProfile(player_id = playerId, sea
 
 seasonInfoByGame= seasonInfo.opponent.get_data_frame()
 print(seasonInfoByGame)
+statDF = seasonInfoByGame.loc[:,statCategory]
+print(statDF)
