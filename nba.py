e69401b340aa9bbab7affc90e5b6297f6c3ffdc1
scott <scott.a.magnuson.19@dartmouth.edu>
2018-09-27 12:15:19 -0400

    added game func

--- nba.py
+++ nba.py
@@ -1,34 +1,21 @@
-from nba_api.stats.endpoints import commonplayerinfo
+from nba_api.stats.endpoints import commonplayerinfo, boxscoreadvancedv2
 import pandas
 
 player_info = commonplayerinfo.CommonPlayerInfo(player_id=2544)
 
 
-
 # Returns the data set in a pandas DataFrame.
-seasonsDF = player_info.available_seasons.get_data_frame()
-print seasonsDF
+avaliableSeasonDF = player_info.available_seasons.get_data_frame()
+print (avaliableSeasonDF.at[0, 'SEASON_ID'])
 
-# # Returns the raw response of the request.
-# print player_info.get_response()
-#
-# # Returns all data in a JSON string.
-# player_info.get_json()
-#
-# # Returns all data in a dictionary.
-# player_info.get_dict()
-#
-# # Returns all data in a normalized JSON string.
-# player_info.get_normalized_json()
-#
-# # Returns all data in a normalized dictionary.
-# player_info.get_normalized_dict()
-#
-# # Returns a list of all data in a pandas DataFrame structure.
-# player_info.get_data_frames()
-#
-#
-#
+playerInfoDF = player_info.common_player_info.get_data_frame()
+
+boxScoreInfo = boxscoreadvancedv2.BoxScoreAdvancedV2(game_id= "0021700807")
+
+boxScoreDF = boxScoreInfo.player_stats.get_data_frame()
+print(boxScoreDF)
+
+
 # # Find players by full name.
 # players.find_players_by_full_name('james')
 #
